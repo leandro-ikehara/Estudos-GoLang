d9a596eb0773898369071ad94ff9a009447e2c2e
leandro-ikehara <leandro.ikehara@gmail.com>
2022-10-08 11:55:12 -0300

    Referência, ponteiros e métodos

--- src/banco/main.go
+++ src/banco/main.go
@@ -9,12 +9,32 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
-	contaDoLeandro := ContaCorrente{titular: "Leandro",
-		numeroAgencia: 589, numeroConta: 65432, saldo: 125.5}
+	contaDaCintia := ContaCorrente{}
+	contaDaCintia.titular = "Silvia"
+	contaDaCintia.saldo = 549.90
 
-	contaDaAna := ContaCorrente{"Ana Paula", 589, 85246, 200.00}
+	fmt.Println(contaDaCintia.saldo)
 
-	fmt.Println(contaDoLeandro)
-	fmt.Println(contaDaAna)
+	fmt.Println(contaDaCintia.Sacar(409.9))
+	fmt.Println(contaDaCintia.saldo)
+
+	// contaDoLeandro := ContaCorrente{titular: "Leandro",
+	//
+	//	numeroAgencia: 589, numeroConta: 65432, saldo: 125.5}
+	//
+	// contaDaAna := ContaCorrente{"Ana Paula", 589, 85246, 200.00}
+	//
+	// fmt.Println(contaDoLeandro)
+	// fmt.Println(contaDaAna)
 }
